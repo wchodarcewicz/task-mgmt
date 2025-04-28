74b6c39769208669c6d190c139bd1c24ef0d798e
Władysław Chodarcewicz <wladyslaw.chodarcewicz@tsh.io>
2025-04-28 11:03:40 +0200

    fix: change project names

--- config/swagger.config.ts
+++ config/swagger.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('swagger', () => ({
   path: process.env.SWAGGER_PATH || 'docs',
-  title: process.env.SWAGGER_TITLE || 'NestJS Boilerplate',
+  title: process.env.SWAGGER_TITLE || 'Project API',
   description: process.env.SWAGGER_DESCRIPTION || 'API docs',
   version: process.env.SWAGGER_VERSION || process.env.npm_package_version,
 }));
